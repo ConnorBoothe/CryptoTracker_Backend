3cc9851b386f92773f124cbfb63a3b4925b6f720
Connor Boothe <connorboothe@gmail.com>
2021-04-06 15:06:08 -0400

    Removed test function, updated AddUser endpoint to POST new user with parameters passed via request body

--- Controllers/AddUser.js
+++ Controllers/AddUser.js
@@ -3,16 +3,10 @@ const router = express.Router();
 
 var User = require('../models/User');
 var users = new User();
-console.log("add user controller")
-users.addUser("Connor", "Boothe", "connorboothe@gmail.com",
-     "Niner96*")
-     .then(function(){
-         console.log("Added user")
-     })
-//send list of my courses
-router.get('/API/AddUser', function(req, res){
-    users.addUser("Connor", "Boothe", "connorboothe@gmail.com",
-     "Niner96*")
+//endpoint to add user to database
+router.post('/API/AddUser', function(req, res){
+    users.addUser(req.body.first_name, re.body.last_name, 
+        req.body.email, req.body.password)
      .then(function(){
          console.log("Added user")
      })
