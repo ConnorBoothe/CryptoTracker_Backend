3f61d70b204d4d998248ed7142c7736ca02f08de
Connor Boothe <connorboothe@gmail.com>
2021-04-09 19:08:04 -0400

    Created API route to check user login credentials

---
+++ Controllers/GetUser.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+var User = require('../models/User');
+var users = new User();
+//endpoint to add user to database
+router.get('/API/GetUser/:email/:password', function(req, res){
+    users.getUserByEmail(req.params.email,req.params.password )
+     .then(function(users){
+         res.send(users)
+     })
+     .catch((err)=>{
+        res.send(err)
+     })
+});
+module.exports = router;
