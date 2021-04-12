900b71948fddf6bdadcef6bf3d4f081a6b21b278
Connor Boothe <connorboothe@gmail.com>
2021-04-11 21:01:35 -0400

    Using body parser to read req body

--- Controllers/AddUser.js
+++ Controllers/AddUser.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended: true,
+    resave: false,
+    saveUninitialized: true
+}));
 var User = require('../models/User');
 var users = new User();
 //endpoint to add user to database
 router.post('/API/AddUser', function(req, res){
-    users.addUser(req.body.first_name, re.body.last_name, 
+    console.log("Trying to add user")
+    console.log(req.body)
+    users.addUser(req.body.first_name, req.body.last_name, 
         req.body.email, req.body.password)
      .then(function(){
          console.log("Added user")
+         res.json("Complete")
      })
 });
 module.exports = router;
